e6c4e757a28dc95146b6b89220970fee3820ca0e
Utkarsh gupta <122604770+Shadowsweep@users.noreply.github.com>
2024-04-19 06:55:21 +0530

    Update settings.py

--- qrgenvrest/settings.py
+++ qrgenvrest/settings.py
@@ -37,7 +37,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "utkarshhg911@gmail.com"
-EMAIL_HOST_PASSWORD = "vbaxmfscozhoxtod"
+EMAIL_HOST_PASSWORD = "your appcode here"
 
 # Application definition
 
